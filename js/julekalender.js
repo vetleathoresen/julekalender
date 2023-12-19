8de5b1e6b286eefbab810d8613fdc56a97db4237
vetleathoresen <143180757+vetleathoresen@users.noreply.github.com>
2023-12-19 14:36:42 +0100

    ferdig produkt
    Ser ikke ut som alle forandringan har blitt lagt te når æ sjekke her, men kalendern ska funk. Vis den ser uferdig ut så gjerne si ifra sånn at æ kan vis dæ ferdig produkt som e heeelt ferdig :)

---
+++ js/julekalender.js
@@ -0,0 +1,430 @@
+
+let luke1Btn = document.querySelector("#Luke1");
+let filmbilde1 = document.getElementById("filmbilde1");
+let today = new Date();
+luke1Btn.onclick = () => {
+    if(today.getDate()>= 1){
+        filmbilde1.style.backgroundImage = 'url("assets/film1.jpeg")';
+        filmbilde1.style.backgroundSize='95%'
+    }
+    else{
+        filmbilde1.style.backgroundImage = 'url("assets/NeiLuke.png")';
+        filmbilde1.style.backgroundSize='95%'
+    }
+}
+
+var element = document.querySelector("#Luke1");
+element.addEventListener("click", toggleDoor);
+function toggleDoor() {
+    this.classList.toggle("doorOpen");
+  }
+
+
+let luke2Btn = document.querySelector("#Luke2");
+let filmbilde2 = document.getElementById("filmbilde2");
+luke2Btn.onclick = () => {
+    if(today.getDate()>= 2){
+        filmbilde2.style.backgroundImage = 'url("assets/film2.png")';
+        filmbilde2.style.backgroundSize='95%'
+    }
+    else{
+        filmbilde2.style.backgroundImage = 'url("assets/NeiLuke.png")';
+        filmbilde2.style.backgroundSize='95%'
+    }
+}
+
+var element = document.querySelector("#Luke2");
+element.addEventListener("click", toggleDoor);
+
+
+let luke3Btn = document.querySelector("#Luke3");
+let filmbilde3 = document.getElementById("filmbilde3");
+luke3Btn.onclick = () => {
+    if(today.getDate()>= 3){
+        filmbilde3.style.backgroundImage = 'url("assets/film3.png")';
+        filmbilde3.style.backgroundSize='95%'
+    }
+    else{
+        filmbilde3.style.backgroundImage = 'url("assets/NeiLuke.png")';
+        filmbilde3.style.backgroundSize='95%'
+    }
+}
+
+var element = document.querySelector("#Luke3");
+element.addEventListener("click", toggleDoor);
+
+
+let luke4Btn = document.querySelector("#Luke4");
+let filmbilde4 = document.getElementById("filmbilde4");
+luke4Btn.onclick = () => {
+    if(today.getDate()>= 4){
+        filmbilde4.style.backgroundImage = 'url("assets/film4.png")';
+        filmbilde4.style.backgroundSize='95%'
+    }
+    else{
+        filmbilde4.style.backgroundImage = 'url("assets/NeiLuke.png")';
+        filmbilde4.style.backgroundSize='95%'
+    }
+}
+
+var element = document.querySelector("#Luke4");
+element.addEventListener("click", toggleDoor);
+
+
+let luke5Btn = document.querySelector("#Luke5");
+let filmbilde5 = document.getElementById("filmbilde5");
+luke5Btn.onclick = () => {
+    if(today.getDate()>= 5){
+        filmbilde5.style.backgroundImage = 'url("assets/film5.png")';
+        filmbilde5.style.backgroundSize='95%'
+    }
+    else{
+        filmbilde5.style.backgroundImage = 'url("assets/NeiLuke.png")';
+        filmbilde5.style.backgroundSize='95%'
+    }
+}
+
+var element = document.querySelector("#Luke5");
+element.addEventListener("click", toggleDoor);
+
+
+
+let luke6Btn = document.querySelector("#Luke6");
+let filmbilde6 = document.getElementById("filmbilde6");
+luke6Btn.onclick = () => {
+    if(today.getDate()>= 6){
+        filmbilde6.style.backgroundImage = 'url("assets/film6.png")';
+        filmbilde6.style.backgroundSize='95%'
+    }
+    else{
+        filmbilde6.style.backgroundImage = 'url("assets/NeiLuke.png")';
+        filmbilde6.style.backgroundSize='95%'
+    }
+}
+
+var element = document.querySelector("#Luke6");
+element.addEventListener("click", toggleDoor);
+
+
+
+let luke7Btn = document.querySelector("#Luke7");
+let filmbilde7 = document.getElementById("filmbilde7");
+luke7Btn.onclick = () => {
+    if(today.getDate()>= 7){
+        filmbilde7.style.backgroundImage = 'url("assets/film7.png")';
+        filmbilde7.style.backgroundSize='95%'
+    }
+    else{
+        filmbilde7.style.backgroundImage = 'url("assets/NeiLuke.png")';
+        filmbilde7.style.backgroundSize='95%'
+    }
+}
+
+var element = document.querySelector("#Luke7");
+element.addEventListener("click", toggleDoor);
+
+
+
+let luke8Btn = document.querySelector("#Luke8");
+let filmbilde8 = document.getElementById("filmbilde8");
+luke8Btn.onclick = () => {
+    if(today.getDate()>= 8){
+        filmbilde8.style.backgroundImage = 'url("assets/film8.png")';
+        filmbilde8.style.backgroundSize='95%'
+    }
+    else{
+        filmbilde8.style.backgroundImage = 'url("assets/NeiLuke.png")';
+        filmbilde8.style.backgroundSize='95%'
+    }
+}
+
+var element = document.querySelector("#Luke8");
+element.addEventListener("click", toggleDoor);
+
+
+
+let luke9Btn = document.querySelector("#Luke9");
+let filmbilde9 = document.getElementById("filmbilde9");
+luke9Btn.onclick = () => {
+    if(today.getDate()>= 9){
+        filmbilde9.style.backgroundImage = 'url("assets/film9.png")';
+        filmbilde9.style.backgroundSize='92%'
+    }
+    else{
+        filmbilde9.style.backgroundImage = 'url("assets/NeiLuke.png")';
+        filmbilde9.style.backgroundSize='95%'
+    }
+}
+
+var element = document.querySelector("#Luke9");
+element.addEventListener("click", toggleDoor);
+
+
+
+let luke10Btn = document.querySelector("#Luke10");
+let filmbilde10 = document.getElementById("filmbilde10");
+luke10Btn.onclick = () => {
+    if(today.getDate()>= 10){
+        filmbilde10.style.backgroundImage = 'url("assets/film10.png")';
+        filmbilde10.style.backgroundSize='82%'
+    }
+    else{
+        filmbilde10.style.backgroundImage = 'url("assets/NeiLuke.png")';
+        filmbilde10.style.backgroundSize='95%'
+    }
+}
+
+var element = document.querySelector("#Luke10");
+element.addEventListener("click", toggleDoor);
+
+
+
+let luke11Btn = document.querySelector("#Luke11");
+let filmbilde11 = document.getElementById("filmbilde11");
+luke11Btn.onclick = () => {
+    if(today.getDate()>= 11){
+        filmbilde11.style.backgroundImage = 'url("assets/film11.png")';
+        filmbilde11.style.backgroundSize='85%'
+    }
+    else{
+        filmbilde11.style.backgroundImage = 'url("assets/NeiLuke.png")';
+        filmbilde11.style.backgroundSize='95%'
+    }
+}
+
+var element = document.querySelector("#Luke11");
+element.addEventListener("click", toggleDoor);
+
+
+
+let luke12Btn = document.querySelector("#Luke12");
+let filmbilde12 = document.getElementById("filmbilde12");
+luke12Btn.onclick = () => {
+    if(today.getDate()>= 12){
+        filmbilde12.style.backgroundImage = 'url("assets/film12.png")';
+        filmbilde12.style.backgroundSize='90%'
+    }
+    else{
+        filmbilde12.style.backgroundImage = 'url("assets/NeiLuke.png")';
+        filmbilde12.style.backgroundSize='95%'
+    }
+}
+
+var element = document.querySelector("#Luke12");
+element.addEventListener("click", toggleDoor);
+
+
+
+let luke13Btn = document.querySelector("#Luke13");
+let filmbilde13 = document.getElementById("filmbilde13");
+luke13Btn.onclick = () => {
+    if(today.getDate()>= 13){
+        filmbilde13.style.backgroundImage = 'url("assets/film13.png")';
+        filmbilde13.style.backgroundSize='86%'
+    }
+    else{
+        filmbilde13.style.backgroundImage = 'url("assets/NeiLuke.png")';
+        filmbilde13.style.backgroundSize='95%'
+    }
+}
+
+var element = document.querySelector("#Luke13");
+element.addEventListener("click", toggleDoor);
+
+
+
+let luke14Btn = document.querySelector("#Luke14");
+let filmbilde14 = document.getElementById("filmbilde14");
+luke14Btn.onclick = () => {
+    if(today.getDate()>= 14){
+        filmbilde14.style.backgroundImage = 'url("assets/film14.png")';
+        filmbilde14.style.backgroundSize='79%'
+    }
+    else{
+        filmbilde14.style.backgroundImage = 'url("assets/NeiLuke.png")';
+        filmbilde14.style.backgroundSize='95%'
+    }
+}
+
+var element = document.querySelector("#Luke14");
+element.addEventListener("click", toggleDoor);
+
+
+
+let luke15Btn = document.querySelector("#Luke15");
+let filmbilde15 = document.getElementById("filmbilde15");
+luke15Btn.onclick = () => {
+    if(today.getDate()>= 15){
+        filmbilde15.style.backgroundImage = 'url("assets/film15.png")';
+        filmbilde15.style.backgroundSize='81%'
+    }
+    else{
+        filmbilde15.style.backgroundImage = 'url("assets/NeiLuke.png")';
+        filmbilde15.style.backgroundSize='95%'
+    }
+}
+
+var element = document.querySelector("#Luke15");
+element.addEventListener("click", toggleDoor);
+
+
+
+let luke16Btn = document.querySelector("#Luke16");
+let filmbilde16 = document.getElementById("filmbilde16");
+luke16Btn.onclick = () => {
+    if(today.getDate()>= 16){
+        filmbilde16.style.backgroundImage = 'url("assets/film16.png")';
+        filmbilde16.style.backgroundSize='95%'
+    }
+    else{
+        filmbilde16.style.backgroundImage = 'url("assets/NeiLuke.png")';
+        filmbilde16.style.backgroundSize='95%'
+    }
+}
+
+var element = document.querySelector("#Luke16");
+element.addEventListener("click", toggleDoor);
+
+
+
+let luke17Btn = document.querySelector("#Luke17");
+let filmbilde17 = document.getElementById("filmbilde17");
+luke17Btn.onclick = () => {
+    if(today.getDate()>= 17){
+        filmbilde17.style.backgroundImage = 'url("assets/film17.png")';
+        filmbilde17.style.backgroundSize='95%'
+    }
+    else{
+        filmbilde17.style.backgroundImage = 'url("assets/NeiLuke.png")';
+        filmbilde17.style.backgroundSize='95%'
+    }
+}
+
+var element = document.querySelector("#Luke17");
+element.addEventListener("click", toggleDoor);
+
+
+
+let luke18Btn = document.querySelector("#Luke18");
+let filmbilde18 = document.getElementById("filmbilde18");
+luke18Btn.onclick = () => {
+    if(today.getDate()>= 18){
+        filmbilde18.style.backgroundImage = 'url("assets/film18.png")';
+        filmbilde18.style.backgroundSize='80%'
+    }
+    else{
+        filmbilde18.style.backgroundImage = 'url("assets/NeiLuke.png")';
+        filmbilde18.style.backgroundSize='95%'
+    }
+}
+
+var element = document.querySelector("#Luke18");
+element.addEventListener("click", toggleDoor);
+
+
+
+let luke19Btn = document.querySelector("#Luke19");
+let filmbilde19 = document.getElementById("filmbilde19");
+luke19Btn.onclick = () => {
+    if(today.getDate()>= 19){
+        filmbilde19.style.backgroundImage = 'url("assets/film19.png")';
+        filmbilde19.style.backgroundSize='86%'
+    }
+    else{
+        filmbilde19.style.backgroundImage = 'url("assets/NeiLuke.png")';
+        filmbilde19.style.backgroundSize='95%'
+    }
+}
+
+var element = document.querySelector("#Luke19");
+element.addEventListener("click", toggleDoor);
+
+
+
+let luke20Btn = document.querySelector("#Luke20");
+let filmbilde20 = document.getElementById("filmbilde20");
+luke20Btn.onclick = () => {
+    if(today.getDate()>= 20){
+        filmbilde20.style.backgroundImage = 'url("assets/film20.png")';
+        filmbilde20.style.backgroundSize='91%'
+    }
+    else{
+        filmbilde20.style.backgroundImage = 'url("assets/NeiLuke.png")';
+        filmbilde20.style.backgroundSize='95%'
+    }
+}
+
+var element = document.querySelector("#Luke20");
+element.addEventListener("click", toggleDoor);
+
+
+
+let luke21Btn = document.querySelector("#Luke21");
+let filmbilde21 = document.getElementById("filmbilde21");
+luke21Btn.onclick = () => {
+    if(today.getDate()>= 21){
+        filmbilde21.style.backgroundImage = 'url("assets/film21.png")';
+        filmbilde21.style.backgroundSize='80%'
+    }
+    else{
+        filmbilde21.style.backgroundImage = 'url("assets/NeiLuke.png")';
+        filmbilde21.style.backgroundSize='95%'
+    }
+}
+
+var element = document.querySelector("#Luke21");
+element.addEventListener("click", toggleDoor);
+
+
+
+let luke22Btn = document.querySelector("#Luke22");
+let filmbilde22 = document.getElementById("filmbilde22");
+luke22Btn.onclick = () => {
+    if(today.getDate()>= 22){
+        filmbilde22.style.backgroundImage = 'url("assets/film22.png")';
+        filmbilde22.style.backgroundSize='84%'
+    }
+    else{
+        filmbilde22.style.backgroundImage = 'url("assets/NeiLuke.png")';
+        filmbilde22.style.backgroundSize='95%'
+    }
+}
+
+var element = document.querySelector("#Luke22");
+element.addEventListener("click", toggleDoor);
+
+
+
+let luke23Btn = document.querySelector("#Luke23");
+let filmbilde23 = document.getElementById("filmbilde23");
+luke23Btn.onclick = () => {
+    if(today.getDate()>= 23){
+        filmbilde23.style.backgroundImage = 'url("assets/film23.png")';
+        filmbilde23.style.backgroundSize='87%'
+    }
+    else{
+        filmbilde23.style.backgroundImage = 'url("assets/NeiLuke.png")';
+        filmbilde23.style.backgroundSize='95%'
+    }
+}
+
+var element = document.querySelector("#Luke23");
+element.addEventListener("click", toggleDoor);
+
+
+
+let luke24Btn = document.querySelector("#Luke24");
+let filmbilde24 = document.getElementById("filmbilde24");
+luke24Btn.onclick = () => {
+    if(today.getDate()>= 24){
+        filmbilde24.style.backgroundImage = 'url("assets/film24.png")';
+        filmbilde24.style.backgroundSize='90%'
+    }
+    else{
+        filmbilde24.style.backgroundImage = 'url("assets/NeiLuke.png")';
+        filmbilde24.style.backgroundSize='95%'
+    }
+}   
+
+var element = document.querySelector("#Luke24");
+element.addEventListener("click", toggleDoor);
